67fe508a872d4f82149f3a970d8465aaaecae1e4
605511287 <605511287@qq.com>
2018-07-24 08:14:45 +0800

    one

--- js/index.js
+++ js/index.js
@@ -14,4 +14,54 @@ window.onload=function () {
             sideTab[i].style.display="none";
         }
     }
+    // 轮播图
+    let bigbox=document.getElementsByClassName("bigbox")[0];
+    let imgbox=bigbox.getElementsByClassName("img")[0];
+    console.log(imgbox);
+    let img=imgbox.getElementsByClassName("img");
+    console.log(img);
+    let forward=imgbox.getElementsByClassName("next")[0];
+    console.log(forward);
+    let back=imgbox.getElementsByClassName("small")[0];
+    console.log(back);
+    let t=setInterval(move,2000);
+    let num=0;
+    function move() {
+        num++;
+        if(num==8){
+            img[num-1].style.zIndex=5;
+            num=0;
+            /*for(let i=0;i<img.length;i++){
+                img[i].style.zIndex=5;
+            }*/
+        }
+        img[num].style.zIndex=10;
+        for(let j=0;j<num;j++){
+            img[j].style.zIndex=5;
+        }
+        // img[num-1].style.zIndex=5;
+    }
+    bigbox.onmouseenter=function () {
+        clearInterval(t);
+    }
+    bigbox.onmouseleave=function () {
+        t=setInterval(move,2000);
+    }
+    forward.onclick=function () {
+        move();
+    }
+    function move2() {
+        num--;
+        if(num<0){
+            img[0].style.zIndex=5;
+            num=7;
+        }
+        img[num].style.zIndex=10;
+        for(let i=7;i>num;i--){
+            img[i].style.zIndex=5;
+        }
+    }
+    back.onclick=function () {
+        move2()
+    }
 }
